fefeed842b7e09c29831d6f9a50ce004f22d6eb2
taha uygun <tahauygun@gmail.com>
2020-01-03 02:30:32 -0800

    setting up server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+
+require('dotenv').config()
+
+
+const app = express;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(cors());
+
+app.get('/api',(req,res)=>{
+  res.json({time: Date().toString()})
+})
+
+const port = process.env.PORT || 8000;
+app.listen(port,()=> {
+  console.log(`Server is running on port ${port}`);
+})
