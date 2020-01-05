57b5b48e71cb95d3546509fe45ac92ed13d474a1
taha uygun <tahauygun@gmail.com>
2020-01-04 19:21:55 -0800

    cleaning up some more bugs

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,24 +6,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true,
     required:true,
-    maxLength:32,
+    max:32,
     unique: true,
     index:true,
-    lowerCase:true
+    lowerase:true
   },
   name:{
     type: String,
     trim: true,
     required:true,
-    maxLength:32,
+    max:32,
   },
   name:{
     type: String,
     trim: true,
     required:true,
-    maxLength:32,
+    max:32,
     unique: true,
-    lowerCase: true
+    lowercase: true
   },
   profile:{
     type:String,
