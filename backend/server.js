f4437b41b570f4455b51b4ead8dbd1790fbacea0
taha uygun <tahauygun@gmail.com>
2020-01-05 23:37:00 -0800

    auth route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config()
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 var app = express()
 
