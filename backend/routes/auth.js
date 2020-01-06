5a907dd9102974538065af4eb67478584ab1072b
taha uygun <tahauygun@gmail.com>
2020-01-05 23:41:57 -0800

    quick fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {signup} = require('../controllers/auth');
 
-router.get('./signup', signup);
+router.post('./signup', signup);
 
 module.exports = router;
 
