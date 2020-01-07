c4500342a0ff34e1cca826de731fbc3b13df3e9c
taha uygun <tahauygun@gmail.com>
2020-01-06 23:30:33 -0800

    finishing authenticate method

--- backend/models/user.js
+++ backend/models/user.js
@@ -66,8 +66,8 @@ userSchema
   });
 
 userSchema.methods = {
-  authenticate: function() {
-
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
   },
   encryptPassword: function (password) {
     if(!password) return ''
