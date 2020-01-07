43534b1fdc266c2c7a321bfc38c491eca312b5fb
taha uygun <tahauygun@gmail.com>
2020-01-06 23:16:56 -0800

    adding auth validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {signup} = require('../controllers/auth');
 
+
+
 router.post('./signup', signup);
 
 module.exports = router;
