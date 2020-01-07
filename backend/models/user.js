11abf1b942ad85978c6457d6c9db51668eda6590
taha uygun <tahauygun@gmail.com>
2020-01-06 23:24:44 -0800

    adding getter and setter for virtual field

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,60 +1,70 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-  username:{
-    type: String,
-    trim: true,
-    required:true,
-    max:32,
-    unique: true,
-    index:true,
-    lowerase:true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      index: true,
+      lowerase: true
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      lowercase: true
+    },
+    profile: {
+      type: String,
+      required: true
+    },
+    hashed_password: {
+      type: String,
+      required: true
+    },
+    salt: String,
+    about: {
+      type: String
+    },
+    role: {
+      type: Number,
+      trim: true
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    },
+    resetPasswordLink: {
+      data: String,
+      default: ""
+    }
   },
-  name:{
-    type: String,
-    trim: true,
-    required:true,
-    max:32,
-  },
-  name:{
-    type: String,
-    trim: true,
-    required:true,
-    max:32,
-    unique: true,
-    lowercase: true
-  },
-  profile:{
-    type:String,
-    required:true
-  },
-  hashed_password:{
-    type:String,
-    required:true
-  },
-  salt:String,
-  about:{
-    type:String
-  },
-  role:{
-    type: Number,
-    trim: true
-  },
-  photo:{
-    data: Buffer,
-    contentType:String
-  },
-  resetPasswordLink:{
-    data: String,
-    default:''
-  }
-},{timestamp:true})
+  { timestamp: true }
+);
 
-userSchema.virtual('password').set(function(password){
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function(){
+    return this._password;
+  });
 
-}).get()
 
 
-
-model.exports = mongoose.model('User', userSchema);
+model.exports = mongoose.model("User", userSchema);
