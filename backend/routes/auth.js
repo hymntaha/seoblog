62c4d52dd966e135ba957e3160c23d440d6d5913
taha uygun <tahauygun@gmail.com>
2020-01-06 23:18:36 -0800

    passing validation into route middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {signup} = require('../controllers/auth');
 
+const {runValidation} = require('../validators');
+const {userSignupValidation} = require('../validators/auth');
 
 
-router.post('./signup', signup);
+router.post('./signup', userSignupValidation, runValidation, signup);
 
 module.exports = router;
 
