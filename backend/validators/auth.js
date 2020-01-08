2e322e134c921defb6f3a1f0f01781b964c64414
taha uygun <tahauygun@gmail.com>
2020-01-07 20:49:12 -0800

    fixing validator

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -7,7 +7,6 @@ exports.userSignupValidator = [
 ]
 
 exports.userSigninValidator = [
-  check('name').not().isEmpty().withMessage('Name is required'),
   check('email').isEmail().withMessage('Must be a valid email address'),
   check('password').isLength({min:6}).withMessage('Password must be at least 6 characters long')
 ]
