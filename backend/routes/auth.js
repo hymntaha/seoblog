efe913fa35a2bfaf4ee73f0265340466c7134d3c
taha uygun <tahauygun@gmail.com>
2020-01-07 20:50:37 -0800

    quick fix for route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../controllers/auth");
-const { signin } = require("../controllers/auth");
+const { signup,signin } = require("../controllers/auth");
 
 const { runValidation } = require("../validators");
 const {
