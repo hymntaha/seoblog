2be51f109b526cb4efeae429f2af365ae1033abe
taha uygun <tahauygun@gmail.com>
2020-01-07 20:48:51 -0800

    adding signin validator

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -5,3 +5,9 @@ exports.userSignupValidator = [
   check('email').isEmail().withMessage('Must be a valid email address'),
   check('password').isLength({min:6}).withMessage('Password must be at least 6 characters long')
 ]
+
+exports.userSigninValidator = [
+  check('name').not().isEmpty().withMessage('Name is required'),
+  check('email').isEmail().withMessage('Must be a valid email address'),
+  check('password').isLength({min:6}).withMessage('Password must be at least 6 characters long')
+]
