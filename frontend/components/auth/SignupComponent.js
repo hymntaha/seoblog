0abb0998b9e4b2dc710b20245f5b66642b57e9d7
taha uygun <tahauygun@gmail.com>
2020-01-11 18:53:10 -0800

    adding signup form

--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const SignupComponent = () => {
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input onChange={handleChange} type="text" className="form-control" placeholder="Enter your name">
+        </div>
+      </form>
+    )
+  }
   return (
     <div>
-      <h2>Signup</h2>
+      {signupForm()}
     </div>
   );
 };
