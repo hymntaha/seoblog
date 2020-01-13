f6efeb4918e0684b3f8a4c5be74a9a3088009468
taha uygun <tahauygun@gmail.com>
2020-01-12 21:23:05 -0800

    finishing handleChange method

--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,47 +1,63 @@
-import React from 'react';
+import React from "react";
 
-const {useState} from 'react';
+import { useState } from "react";
 
 const SignupComponent = () => {
   const [values, setValues] = useState({
-    name: '',
-    email: '',
-    password: '',
-    error: '',
+    name: "",
+    email: "",
+    password: "",
+    error: "",
     loading: false,
-    message:'',
-    showForm:true
+    message: "",
+    showForm: true
   });
 
-  const { name, email, password ,error, loading, message, showForm} = values;
+  const { name, email, password, error, loading, message, showForm } = values;
 
-  const handleSubmit = (e)=> {
+  const handleSubmit = e => {
     e.preventDefault();
-    console.log('handle submit')
-  }
-  const handleChange = (e)=> {
-   console.log(e.target.value)
-  }
+    console.log("handle submit");
+  };
+  const handleChange = e => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
   const signupForm = () => {
     return (
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-        <input onChange={handleChange} type="text" className="form-control" placeholder="Enter your name" />
-      </div>
-        <div className="form-group">
-          <input onChange={handleChange} type="email" className="form-control" placeholder="Enter your email" />
+          <input
+            value={name}
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+            placeholder="Enter your name"
+          />
         </div>
         <div className="form-group">
-          <input onChange={handleChange} type="password" className="form-control" placeholder="Enter your password" />
+          <input
+            value={email}
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+          />
         </div>
-        <button className="btn btn-primary">
-          Signup
-        </button>
+        <div className="form-group">
+          <input
+            value={password}
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button className="btn btn-primary">Signup</button>
       </form>
-    )
+    );
   };
 
-  return <React.Fragment>{signupForm()}</React.Fragment>
+  return <React.Fragment>{signupForm()}</React.Fragment>;
 };
 
 export default SignupComponent;
