d8639cf14e1591dd0250876698b6d9bff6c1bc63
taha uygun <tahauygun@gmail.com>
2020-01-13 22:12:34 -0800

    creating signin comp

---
+++ frontend/components/auth/SigninComponent.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+import { useState } from "react";
+import { signup } from "../../actions/auth";
+
+const SigninCompnent = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    message: "",
+    showForm: true
+  });
+
+  const { name, email, password, error, loading, message, showForm } = values;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setValues({ ...values, loading: true, error: false });
+    const user = { name, email, password };
+
+    signup(user).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          password: "",
+          email: "",
+          error: "",
+          loading: false,
+          message: data.message,
+          showForm: false
+        });
+      }
+    });
+  };
+  const handleChange = e => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const showLoading = () =>
+    loading ? <div className="aler alert-info">Loading...</div> : "";
+  const showError = () =>
+    error ? <div className="aler alert-info">{error}</div> : "";
+  const showMessage = () =>
+    message ? <div className="aler alert-info">{message}</div> : "";
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            value={name}
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            value={email}
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            value={password}
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button className="btn btn-primary">Signup</button>
+      </form>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      {showError()}
+      {showLoading()}
+      {showMessage()}
+      {showForm && signupForm()}
+    </React.Fragment>
+  );
+};
+
+export default SigninCompnent;
