1aefd8cd18d0bbf1119bdc1f8fe4c2f0191463d6
taha uygun <tahauygun@gmail.com>
2020-01-14 23:04:18 -0800

    adding signin component to signin page

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,15 @@
 import Layout from '../components/Layout';
+import SigninCompnent from "../components/auth/SigninComponent";
 
 const Signin = () => {
   return (
     <Layout>
-      <h2>Signin page</h2>
+      <h2 className="text-center pt-4 pb-4">Sign up page</h2>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <SigninCompnent/>
+        </div>
+      </div>
     </Layout>
   );
 };
