752fd69365ea43f2aa8e831090b480fc67388f4e
taha uygun <tahauygun@gmail.com>
2020-01-16 22:19:27 -0800

    adding user.js route

---
+++ backend/routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { signup,signin,signout } = require("../controllers/auth");
+
+const { runValidation } = require("../validators");
+const {
+  userSignupValidation,
+  userSigninValidation
+} = require("../validators/auth");
+
+router.post("/signup", userSignupValidation, runValidation, signup);
+router.post("/signin", userSigninValidation, runValidation, signin);
+router.get("/signout", signout);
+
+router.get('/')
+
+
+module.exports = router;
