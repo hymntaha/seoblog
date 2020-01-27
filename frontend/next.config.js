cc6d4b1d2675033b1049d25678223a8d053e0921
taha uygun <tahauygun@gmail.com>
2020-01-26 22:28:15 -0800

    adding some config for static directory

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const withCss = require('@zeit/next-css');
+
+module.exports = withCss({
   publicRuntimeConfig: {
     APP_NAME: 'SEOBLOG',
     API_DEVELOPMENT:'http://localhost:8000/api',
     PRODUCTION: false
   }
-}
+})
