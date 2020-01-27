081d76e6fd2713eecc81ca33b6c1708ab09c7612
taha uygun <tahauygun@gmail.com>
2020-01-26 22:40:12 -0800

    category page

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,11 +1,23 @@
-import Layout from '../../components/Layout';
-import Admin from '../../components/auth/Admin';
+import Layout from "../../components/Layout";
+import Admin from "../../components/auth/Admin";
 
 const AdminIndex = () => {
   return (
     <Layout>
       <Admin>
-        <h2>Admin Dashboard</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>Admin Dashboard</h2>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-group">
+                <li className="list-group-item"><Link href='/crud/category-tag'></Link></li>
+              </ul>
+            </div>
+            <div className="col-md-8" />
+          </div>
+        </div>
       </Admin>
     </Layout>
   );
