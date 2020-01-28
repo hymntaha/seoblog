b0375367a5429aec904cb37155f24a1cd865123c
taha uygun <tahauygun@gmail.com>
2020-01-27 23:12:08 -0800

    Category component creation

---
+++ frontend/components/crud/Category.js
@@ -0,0 +1,11 @@
+import {useSTate, useEffect} from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+import {isAuth, getCookie} from "../../actions/auth";
+import {create} from "../../actions/category";
+
+const Category = () =>{
+
+}
+
+export default Category;
